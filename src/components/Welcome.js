e07b529405337144d90842037b808daff362a0b6
shubhamchandge <shubhamchandge@gmail.comm>
2023-03-17 10:34:10 +0530

    solved

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,10 +2,10 @@
 
 import React from 'react'
 
-const Welcome = (props) => {
+const Welcome = ({name}) => {
   return (
     <div>
-      <h1> Hey ! {props.name} </h1>
+      <h1>  Hey !  {name} </h1>
       <h2>Welcome to school.</h2>
     </div>
   )
